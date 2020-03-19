81f3b7256a7aafcae3f89e09dcff800e2e28c64e
Matthew Tham <matthewtham@Matthews-MacBook-Pro.local>
2020-03-19 10:46:28 +0800

    Added new item placing API

--- shelfer.R
+++ shelfer.R
@@ -1,15 +1,69 @@
-#ffff00 = Target objects (for now, the first target for all agents)
-## Can be placed in any shelf position (so every wall tile minus the entrance wall)
-shelf_mat <- store_layout$walls_mat
-# Zero out the entrance wall
-shelf_mat[nrow(shelf_mat),] <- 0
-# Convert the available spots into df format for sampling
-shelf_df <- make_df(shelf_mat, 1)
-shelf_positions <- shelf_df[sample(nrow(shelf_df), 134),]
-shelf_positions<-shelf_positions[order(shelf_positions$y),]
-#Sort Item catalog by happiness
-storedata_sorted<-storedata[order(storedata$ghi),]
-shelf_positions$item_id <- storedata_sorted$item_id
-shelf_positions$ghi <- storedata_sorted$ghi
-shelf_positions <- shelf_positions[order(shelf_positions$item_id),]
-store_layout$target_df<-shelf_positions
+# Get the store_layout for editing
+store_layout <- read_img_map("example.pbm")
+
+# Core place_items function; no need to edit this code
+place_items <- function(store_layout, storedata, select_positions, order_positions, order_items) {
+  shelf_mat <- store_layout_ascending$walls_mat
+  # Zero out the entrance wall
+  shelf_mat[nrow(shelf_mat),] <- 0
+  # Convert the available spots into df format for sampling
+  shelf_df <- make_df(shelf_mat, 1)
+  # Select 134 shelf positions from possible positions
+  shelf_positions <- select_positions(shelf_df)
+  # Insert an item into each shelf position
+  ## Sort positions by increasing y position
+  shelf_positions<-order_positions(shelf_positions)
+  ## Sort items by increasing GHI
+  storedata_sorted<-order_items(storedata)
+  ## Bind positions to items
+  shelf_positions$value <- storedata_sorted$item_id
+  shelf_positions$ghi <- storedata_sorted$ghi
+  shelf_positions <- shelf_positions[order(shelf_positions$value),]
+  store_layout$target_df<-shelf_positions
+  return(store_layout)
+}
+
+# Some example selection/ordering functions (you'll have to write more of these yourself)
+
+# A select_position function takes in a shelf_df, and selects 134 positions from it somehow
+select_positions_random <- function(shelf_df) {
+  shelf_positions <- shelf_df[sample(nrow(shelf_df), 134),]
+  return(shelf_positions)
+}
+
+# An order_position function takes in a shelf_positions df with 134 rows, and orders them somehow
+order_positions_ascending_y <- function(shelf_positions) {
+  shelf_positions<-shelf_positions[order(shelf_positions$y),]
+  return(shelf_positions)
+}
+
+order_positions_descending_y <- function(shelf_positions) {
+  shelf_positions<-shelf_positions[order(-shelf_positions$y),]
+  return(shelf_positions)
+}
+
+# An order_items function sorts store_data based on some attribute (e.g. ghi, fragility, wtvr else)
+order_items_ascending_ghi <- function(storedata) {
+  storedata_sorted<-storedata[order(storedata$ghi),]
+  return(storedata_sorted)
+}
+
+order_items_random <- function(storedata) {
+  storedata_sorted <- storedata[sample(storedata$item_id),]
+}
+## An example call to place_items(): 
+## Concept is that you tell place_items() how to select shelves from the walls (select_positions()), how to order those positions according to coordinates (order_positions()), and how to insert item_ids into the sorted coordinates (order_items())
+
+# test_store_layout <- place_items(store_layout, storedata, select_positions_random, order_positions_descending_y, order_items_ascending_ghi)
+
+## After creating a new store layout, pass it to the simulate function to get your loss. 
+
+# n_reps = 5
+# results_descending = list()
+# for(i in 1:n_reps) {
+#   results_descending[[i]] <- simulate(store_layout = store_layout_descending, storedata=storedata, n_agents = 100, plot=FALSE, from_bitmap = FALSE)
+# }
+
+## NOTE: PLEASE RUN YOUR EXPERIMENTS IN A SEPARATE FILE TO AVOID DELETING CORE FUNCTIONS. LOOK INTO tham_experiments.R TO SEE HOW YOU CAN ORGANISE EXPERIMENTS
+
+# Either add new selection/ordering functions in this file, and push to the git, or write them in your own experiments file
--- tham_experiments.R
+++ tham_experiments.R
@@ -1,31 +1,38 @@
 source("simulate.R", local=TRUE)
+source("shelfer.R", local=TRUE)
+
 n_reps = 5
 
-# Baseline: completely randomised arrangement of items
-results = list()
-
-for(i in 1:n_reps) {
-  results[[i]] <- simulate("example.pbm", storedata=storedata, n_agents = 100, plot=FALSE)
-}
-
-# Get the store_layout for editing
+# Pull a store_layout from an image
 store_layout <- read_img_map("example.pbm")
-store_layout_ascending <- store_layout
-shelf_mat <- store_layout_ascending$walls_mat
-# Zero out the entrance wall
-shelf_mat[nrow(shelf_mat),] <- 0
-# Convert the available spots into df format for sampling
-shelf_df <- make_df(shelf_mat, 1)
-shelf_positions <- shelf_df[sample(nrow(shelf_df), 134),]
-shelf_positions<-shelf_positions[order(shelf_positions$y),]
-#Sort Item catalog by happiness
-storedata_sorted<-storedata[order(storedata$ghi),]
-shelf_positions$item_id <- storedata_sorted$item_id
-shelf_positions$ghi <- storedata_sorted$ghi
-shelf_positions <- shelf_positions[order(shelf_positions$item_id),]
-store_layout_ascending$target_df<-shelf_positions
 
+# Baseline: completely randomised arrangement of items
+# Place items into the layout
+store_layout_random <- place_items(store_layout, storedata, select_positions_random, order_positions_ascending_y, order_items_random)
+# Create a blank list to store results
+results = list()
+# Run simulation for 5 reps
+for(i in 1:n_reps) {
+  results[[i]] <- results_descending[[i]] <- simulate(store_layout = store_layout_descending, storedata=storedata, n_agents = 100, plot=FALSE, from_bitmap = FALSE)
+}
+# Save those results in an .rds file for later use
+saveRDS(results, "default.rds")
+
+
+# Ascending (i.e. more desirable items farther away)
+store_layout_ascending <- place_items(store_layout, storedata, select_positions_random, order_positions_ascending_y, order_items_ascending_ghi)
 results_ascending = list()
 for(i in 1:n_reps) {
-  results_ascending[i] <- simulate(store_layout=store_layout, storedata=storedata, n_agents = 100, plot=FALSE, from_bitmap = FALSE)
+  results_ascending[[i]] <- simulate(store_layout = store_layout_ascending, storedata=storedata, n_agents = 100, plot=FALSE, from_bitmap = FALSE)
 }
+saveRDS(results_ascending, "default_ascending.rds")
+
+# Descending
+store_layout_descending <- place_items(store_layout, storedata, select_positions_random, order_positions_descending_y, order_items_ascending_ghi)
+results_descending = list()
+for(i in 1:n_reps) {
+  results_descending[[i]] <- simulate(store_layout = store_layout_descending, storedata=storedata, n_agents = 100, plot=FALSE, from_bitmap = FALSE)
+}
+saveRDS(results_descending, file="default_descending.rds")
+
+
