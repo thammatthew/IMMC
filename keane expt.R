64fdbe5e7b0bc3c7ab94cd5e7e3a91fef55fc70a
keane tan <donkzwonkz@gmail.com>
2020-03-20 20:02:55 +0800

    Euc_entrance

--- keane expt.R
+++ keane expt.R
@@ -9,7 +9,7 @@ store_layout <- read_img_map("store_layout/bitmap/original_layout_blocked.pbm")
 
 # Baseline: completely randomised arrangement of items
 # Place items into the layout
-store_layout_random <- place_items(store_layout, storedata, select_positions_random, order_positions_euclidean, order_items_ascending_ghi,threshold=0,x=35,y=48)
+store_layout_random <- place_items(store_layout, storedata, select_positions_random, order_positions_euclidean, order_items_ascending_ghi,threshold=0,x=38,y=48)
 
 # Create a blank list to store results
 results = list()
@@ -17,27 +17,12 @@ results = list()
 for(i in 1:n_reps) {
   results[[i]] <- simulate(store_layout = store_layout_random, storedata=storedata, n_agents = n_agents, plot=FALSE, from_bitmap = FALSE)
 }
-# Save those results in an .rds file for later use
-saveRDS(results, "default_euc_entrance.rds")
 
 
-# Ascending (i.e. more desirable items farther away)
-store_layout_ascending <- place_items(store_layout, storedata, select_positions_random, order_positions_ascending_y, order_items_ascending_ghi)
-results_ascending = list()
-for(i in 1:n_reps) {
-  results_ascending[[i]] <- simulate(store_layout = store_layout_ascending, storedata=storedata, n_agents = n_agents, plot=FALSE, from_bitmap = FALSE)
-}
-saveRDS(results_ascending, "default_ascending.rds")
-
-# Descending
-store_layout_descending <- place_items(store_layout, storedata, select_positions_random, order_positions_descending_y, order_items_ascending_ghi)
-results_descending = list()
-for(i in 1:n_reps) {
-  results_descending[[i]] <- simulate(store_layout = store_layout_descending, storedata=storedata, n_agents = n_agents, plot=FALSE, from_bitmap = FALSE)
-}
-saveRDS(results_descending, file="default_descending.rds")
+#save
 output <- list(store_layout_random, results)
-plot_output(output, "default_descending")
-plot_mat(results[1][1])
+saveRDS(output, "default_euc_entrance.rds")
+plot_output(output, "default_euc_entrance")
+
 
 
