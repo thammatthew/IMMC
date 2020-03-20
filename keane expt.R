1da583093848a1acf0322eaef9516fc43648dc34
keane tan <donkzwonkz@gmail.com>
2020-03-20 21:52:02 +0800

    static

--- keane expt.R
+++ keane expt.R
@@ -9,7 +9,7 @@ store_layout <- read_img_map("store_layout/bitmap/original_layout_blocked.pbm")
 
 # Baseline: completely randomised arrangement of items
 # Place items into the layout
-store_layout_random <- place_items(store_layout, storedata, select_positions_random, order_positions_euclidean, order_items_ascending_ghi,threshold=0,x=38,y=48)
+store_layout_random <- place_items(store_layout, storedata, select_positions_static, order_positions_euclidean, order_items_ascending_ghi,threshold=0.9,x=23,y=38)
 
 # Create a blank list to store results
 results = list()
@@ -21,8 +21,10 @@ for(i in 1:n_reps) {
 
 #save
 output <- list(store_layout_random, results)
-saveRDS(output, "default_euc_entrance.rds")
-plot_output(output, "default_euc_entrance")
+saveRDS(output, "default_euc_cashier_0.75.rds")
+plot_output(output, "default_euc_cashier_0.75")
 
 
 
+saveRDS(store_layout,"storelayout.rds")
+plot_mat(store_layout$blocked_mat)
